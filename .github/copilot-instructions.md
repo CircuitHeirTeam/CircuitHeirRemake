57f760334cfbef81df20a192bf718dc9b5671fd9
SayonaraPotato <vmaster093@gmail.com>
2025-06-03 15:06:55 +0500

    Joint Connect first iteration remake
    Детали соединяются, осталось доделать расположение относительно друг друга и отключить коллизию у соединяемого объекта (только один grab на объект

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: CircuitHeirRemake
 - Unity version: Unity 6000.0.49f1
 - Active game object:
-  - Name: Joint collider
+  - Name: Connector
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/ItemConnector.cs
+++ Assets/Scripts/ItemConnector.cs
@@ -2,13 +2,25 @@ using UnityEngine;
 
 public class ItemConnector : MonoBehaviour
 {
-    [SerializeField] private FixedJoint _joint;
+    private void Start()
+    {
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "Cylinder")
         {
+            other.transform.SetParent(transform.root.gameObject.transform);
+
+            other.transform.position = this.transform.position;
+            other.transform.localEulerAngles = new Vector3(0, 0, 0);
+
+            FixedJoint _joint = transform.root.gameObject.AddComponent<FixedJoint>();
             _joint.connectedBody = other.attachedRigidbody;
+
+
         }
+        Destroy(this);
     }
 }
