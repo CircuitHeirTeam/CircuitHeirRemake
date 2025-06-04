aa5b489f07f95210ffbdd4e6a0524f5a902c8788
SayonaraPotato <vmaster093@gmail.com>
2025-06-04 15:38:38 +0500

    Item connect itteration 2
    Вроде пофиксил соединение предметов
    Отцепил одну ногу чтобы начать тестировать соединения непосредственно на роботе
    Заебался

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -2,7 +2,7 @@
 - Project name: CircuitHeirRemake
 - Unity version: Unity 6000.0.49f1
 - Active game object:
-  - Name: Connector
+  - Name: Bip001 R HorseLink
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/ItemConnector.cs
+++ Assets/Scripts/ItemConnector.cs
@@ -1,25 +1,22 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class ItemConnector : MonoBehaviour
 {
-    private void Start()
-    {
-
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "Cylinder")
         {
+            //калибровка расположения обьектов относительно друг друга
             other.transform.SetParent(transform.root.gameObject.transform);
-
             other.transform.position = this.transform.position;
             other.transform.localEulerAngles = new Vector3(0, 0, 0);
 
             FixedJoint _joint = transform.root.gameObject.AddComponent<FixedJoint>();
-            _joint.connectedBody = other.attachedRigidbody;
 
+            _joint.connectedBody = other.attachedRigidbody; //коннект двух объектов
 
+            other.GetComponent<XRGrabInteractable>().enabled = false;
         }
         Destroy(this);
     }
