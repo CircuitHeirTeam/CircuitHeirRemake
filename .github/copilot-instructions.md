2830a8b02c8537b484740b5bffda3cc281586b65
BashCooler <den.tryaskin@mail.ru>
2025-06-06 02:09:28 +0500

    Joint patch, XR Device Simulator implementation
    1. Соединение предметов теперь работает.
    2. XR Interaction Toolkit обновлён до версии 3.1.2
    3. Добавлен XR Device Simulator, позволяющий запускать игру без VR очков (при возникновении проблем с VR отключить объект XR Device Simulator, больше ничего не затронуто).
    3. Добавлен кран к которому можно прикрепить тулово робота

--- .github/copilot-instructions.md
+++ .github/copilot-instructions.md
@@ -1,6 +1,12 @@
 <!-- UNITY CODE ASSIST INSTRUCTIONS START -->
 - Project name: CircuitHeirRemake
 - Unity version: Unity 6000.0.49f1
+- Active scene:
+  - Name: BasicScene
+  - Tags:
+    - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Anchor
+  - Layers:
+    - Default, TransparentFX, Ignore Raycast, Water, UI, Robot Parts
 - Active game object:
   - Name: Connector
   - Tag: Untagged
--- Assets/Scripts/ItemConnector.cs
+++ Assets/Scripts/ItemConnector.cs
@@ -3,19 +3,28 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class ItemConnector : MonoBehaviour
 {
+    [SerializeField] private GameObject objectToConnect;
+
+    [SerializeField] private Vector3 offset;
+    
+    [SerializeField] private Vector3 rotation;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Cylinder")
+        if (other.gameObject == objectToConnect)
         {
-            //калибровка расположения обьектов относительно друг друга
+            GameObject parent = this.transform.parent.gameObject;
+            
+            //РєР°Р»РёР±СЂРѕРІРєР° СЂР°СЃРїРѕР»РѕР¶РµРЅРёСЏ РѕР±СЉРµРєС‚РѕРІ РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ РґСЂСѓРі РґСЂСѓРіР°
             other.transform.SetParent(transform.root.gameObject.transform);
-            other.transform.position = this.transform.position;
-            other.transform.localEulerAngles = new Vector3(0, 0, 0);
-
-            FixedJoint _joint = transform.root.gameObject.AddComponent<FixedJoint>();
-
-            _joint.connectedBody = other.attachedRigidbody; //коннект двух объектов
-
+            other.transform.position = this.transform.position - offset;
+            other.transform.localEulerAngles = rotation;
+            
+            // РЎРѕРµРґРёРЅРµРЅРёРµ
+            var joint = parent.AddComponent<FixedJoint>();
+            joint.connectedBody = other.GetComponent<Rigidbody>();
+            
+            // Р’С‚РѕСЂРѕР№ РїСЂРµРґРјРµС‚ Р±РѕР»СЊС€Рµ РЅРµ С‚СЏРіР°С‚СЊ
             other.GetComponent<XRGrabInteractable>().enabled = false;
         }
         Destroy(this);
